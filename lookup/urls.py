1d7d084c596eadf3657473bf78ad6dce62efc790
Jason Edwards <edwardj3@sky.com>
2023-03-25 20:37:38 +0000

    second commit

--- lookup/urls.py
+++ lookup/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=F3ECAC13-90C4-4ADD-93D3-7284B0A5138A
     path('', views.home, name="home"),
     path('about', views.about, name="about"),
 ]
--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=F3ECAC13-90C4-4ADD-93D3-7284B0A5138A")
+
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+   
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
